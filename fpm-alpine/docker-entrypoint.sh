e31649e0104d446e9f2b12b6c397c57f55ded7a1
sklinux <sklinux@qq.com>
2021-08-06 16:11:33 +0800

    Update docker-entrypoint.sh

--- fpm-alpine/docker-entrypoint.sh
+++ fpm-alpine/docker-entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sed -i 's/1440/14400/g' /var/www/html/libraries/config.default.php
-echo "session.gc_maxlifetime=14400" > /usr/local/etc/php/php.ini
-echo "<?php phpinfo(); ?>" >> /var/www/html/sk.php
 
 if [[ "$1" == apache2* ]] || [ "$1" == php-fpm ]; then
 
