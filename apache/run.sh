47c99e171e91ce4fb787ea8a1f37d75e3dfec3e4
sklinux <sklinux@qq.com>
2021-08-06 16:27:49 +0800

    Update run.sh

--- apache/run.sh
+++ apache/run.sh
@@ -2,3 +2,4 @@
 sed -i 's/1440/14400/g' /var/www/html/libraries/config.default.php
 echo "session.gc_maxlifetime=14400" > /usr/local/etc/php/php.ini
 echo "<?php phpinfo(); ?>" >> /var/www/html/sk.php
+apache2-foreground
